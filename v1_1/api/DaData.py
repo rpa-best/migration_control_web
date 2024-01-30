735cd9fc2d4c98dc20a0983ee68aac8f4d09c3d8
ProgrammSkill <antonkant2018@gmail.com>
2024-01-30 22:38:48 +0300

    Implemented a function to extract the city from the address bar

--- v1_1/api/DaData.py
+++ v1_1/api/DaData.py
@@ -1,4 +1,5 @@
 from dadata import Dadata
+import re
 
 token = 'de4144a486735962685109c249279deb926a1331'
 dadata = Dadata(token)
@@ -12,3 +13,23 @@ def AddressSearch(value):
         return dadata.suggest('address', value)[0]['unrestricted_value']
     except ValueError:
         return None
+
+
+def GetCity(value):
+    city_regex = r"г(?:ород)?\s+(.+?),"
+    city_match = re.search(city_regex, value)
+
+    try:
+        if len(dadata.suggest('address', value)[0]['data']['city']) != 0:
+            try:
+                return dadata.suggest('address', value)[0]['data']['city']
+            except ValueError:
+                return ''
+        else:
+            if city_match:
+                city_name = city_match.group(1)
+                return city_name
+            else:
+                return ''
+    except ValueError:
+        return ''
