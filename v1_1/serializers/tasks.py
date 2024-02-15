d9ad39f7e419859885910e7fe407f527316e0123
ProgrammSkill <antonkant2018@gmail.com>
2024-02-15 10:43:55 +0300

    Implemented a serializer for the list of expired documents

--- v1_1/serializers/tasks.py
+++ v1_1/serializers/tasks.py
@@ -1,9 +1,28 @@
 from rest_framework import serializers
-
 from v1_1.models.worker import DocumentsWorker
 
 
 class DocumentsWorkerSerializer(serializers.ModelSerializer):
+    document_id = serializers.IntegerField()
+    document_type = serializers.CharField()
+    worker_id = serializers.IntegerField()
+    worker = serializers.CharField()
+    organization_id = serializers.IntegerField()
+    organization = serializers.CharField()
+    expiration_date = serializers.DateField()
+    days_until_expiration = serializers.CharField()
+    recommended_start_date = serializers.DateField()
+
     class Meta:
         model = DocumentsWorker
-        fields = '__all__'
+        fields = (
+            'document_id',
+            'document_type',
+            'worker_id',
+            'worker',
+            'organization_id',
+            'organization',
+            'expiration_date',
+            'days_until_expiration',
+            'recommended_start_date'
+        )
--- v1_1/views/tasks.py
+++ v1_1/views/tasks.py
@@ -1,12 +1,18 @@
 from rest_framework import mixins, viewsets
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from datetime import datetime, timedelta
 from v1_1.models import OrganizationUser
 from v1_1.models.worker import DocumentsWorker
 from django.db.models import Q
+from v1_1.serializers.tasks import DocumentsWorkerSerializer
 
 
 class ExpiringDocumentsView(mixins.ListModelMixin, viewsets.GenericViewSet):
+    serializer_class = DocumentsWorkerSerializer
+    permission_class = IsAuthenticated
+
     def list(self, request, **kwargs):
         user = request.user  # Получение авторизованного пользователя
 
@@ -17,6 +23,7 @@ class ExpiringDocumentsView(mixins.ListModelMixin, viewsets.GenericViewSet):
         except OrganizationUser.DoesNotExist:
             return Response({'error': 'Вы не связаны ни с какой организацией'}, status=400)
 
+        # Текущая дата
         today = datetime.now().date()
         # Необходимо, чтобы возвращались документы, которым остаётся 30 дней до окончания срока или которые уже
         # просрочены
@@ -33,6 +40,7 @@ class ExpiringDocumentsView(mixins.ListModelMixin, viewsets.GenericViewSet):
             else:
                 days_until_expiration = (doc.date_end - today).days
 
+            # Рекомендуемая дата начала оформления документа
             recommended_start_date = doc.date_end - timedelta(days=7)
 
             worker_info = {
@@ -40,6 +48,7 @@ class ExpiringDocumentsView(mixins.ListModelMixin, viewsets.GenericViewSet):
                 'document_type': doc.get_type_document_display(),
                 'worker_id': doc.worker_id.id,
                 'worker': f'{doc.worker_id.surname} {doc.worker_id.name} {doc.worker_id.patronymic or ''}',
+                'organization_id': doc.worker_id.organization.id,
                 'organization': f'{doc.worker_id.organization.get_organizational_form_display()} '
                                 f'{doc.worker_id.organization.name}',
                 'expiration_date': doc.date_end,
