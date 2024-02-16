a5980636239dae0ef6438d9684597ef4737606c7
ProgrammSkill <antonkant2018@gmail.com>
2024-02-16 12:57:47 +0300

    Amendments to subscription models

--- v1_1/models/subscription.py
+++ v1_1/models/subscription.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.db import models
 from v1_1.models.user import User
 
@@ -5,9 +6,8 @@ from v1_1.models.user import User
 class ServiceRate(models.Model):
     model_name = models.SlugField(unique=True)
     name = models.CharField(max_length=255)
-    number_companies = models.IntegerField(default=0)
-    number_employees = models.IntegerField(default=0)
-    cost = models.FloatField(default=0)
+    cost_organizations = models.FloatField(default=0)
+    cost_workers = models.FloatField(default=0)
 
     def __str__(self) -> str:
         return self.model_name
@@ -24,3 +24,9 @@ class Subscription(models.Model):
     status = models.CharField(max_length=50, default='process', choices=STATUS)
     user = models.ForeignKey('User', models.CASCADE, to_field='username')
     service_rate = models.ForeignKey(ServiceRate, models.CASCADE, to_field='model_name')
+    number_organizations = models.IntegerField(default=0)
+    number_workers = models.IntegerField(default=0)
+    expiration_date = models.DateField()
+
+    def is_active(self):
+        return self.expiration_date > datetime.now().date()
