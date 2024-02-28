76a0a7b3548ca7c5dcebbfff32a4d0f7434ee844
ProgrammSkill <antonkant2018@gmail.com>
2024-02-28 16:21:51 +0300

    The `News` model has been created

--- v1_1/models/__init__.py
+++ v1_1/models/__init__.py
@@ -1,7 +1,8 @@
 from .user import User
 from .organization import Bank, OrganizationUser
 from .subscription import Subscription, checking_subscription_relevance
+from .news import News
 
 __all__ = [
-    'User', 'Bank', 'OrganizationUser', 'Subscription'
+    'User', 'Bank', 'OrganizationUser', 'Subscription', 'News'
 ]
---
+++ v1_1/models/news.py
@@ -0,0 +1,12 @@
+from django.db import models
+from v1_1.common_utils.file_paths import UploadPath
+from v1_1.models.organization import Organization
+from v1_1.models.user import User
+from django.utils import timezone
+
+
+class News(models.Model):
+    title = models.CharField(max_length=255, blank=True)
+    content = models.TextField()
+    date_publication = models.DateTimeField(default=timezone.now, null=True)
+    author = models.ForeignKey(User, models.CASCADE, blank=True, null=True)
