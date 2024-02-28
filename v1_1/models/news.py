ffb94ceac56ccfa6edfbf5f3aa5f6b92741b10ae
ProgrammSkill <antonkant2018@gmail.com>
2024-02-28 16:38:33 +0300

    Implemented the `Tags` model for news

--- v1_1/models/news.py
+++ v1_1/models/news.py
@@ -5,8 +5,13 @@ from v1_1.models.user import User
 from django.utils import timezone
 
 
+class Tags(models.Model):
+    name = models.CharField(max_length=100, blank=True)
+
+
 class News(models.Model):
     title = models.CharField(max_length=255, blank=True)
     content = models.TextField()
+    tag = models.ForeignKey(Tags, on_delete=models.CASCADE)
     date_publication = models.DateTimeField(default=timezone.now, null=True)
     author = models.ForeignKey(User, models.CASCADE, blank=True, null=True)
