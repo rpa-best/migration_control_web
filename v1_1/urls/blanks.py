149c3a03eb0af933fdd763a53cced4b6ce0b9a3a
ProgrammSkill <antonkant2018@gmail.com>
2024-03-05 17:59:36 +0300

    The search for employees has been implemented

--- v1_1/urls/blanks.py
+++ v1_1/urls/blanks.py
@@ -1,9 +1,14 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
 from v1_1.views.blanks import NoticeConclusionAPIView, EmploymentContractAPIView, SuspensionOrderAPIView, \
-    PaymentOrderAPIView, ContractProvisionPaidServicesAPIView
+    PaymentOrderAPIView, ContractProvisionPaidServicesAPIView, SearchWorkers
 
+
+router = routers.DefaultRouter()
+router.register('search-workers/', SearchWorkers, 'search_workers')
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('employment-contract/', EmploymentContractAPIView.as_view()),
     path('suspension-order/', SuspensionOrderAPIView.as_view()),
     path('payment-order/', PaymentOrderAPIView.as_view()),
