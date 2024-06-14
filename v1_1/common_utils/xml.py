3e3e4ec66b5d0f81b3380aa5ad5290b1d5f11973
ProgrammSkill <antonkant2018@gmail.com>
2024-06-14 12:03:52 +0300

    Code optimization

--- v1_1/common_utils/xml.py
+++ v1_1/common_utils/xml.py
@@ -73,162 +73,28 @@ def json_to_xml(workers: list) -> str:
         documents_worker = ET.SubElement(row_worker, 'Documents', attrib={'Index': '0'})
 
         documents = DocumentsWorker.objects.filter(worker_id=workers[i]['id'], archive=False)
+
+        documents_dict = {
+            'passport': ('Passport', ['Series', 'Number', 'IssuedWhom', 'DateIssue', 'DateEnd']),
+            'migration_card': ('MigrationCard', ['Series', 'Number', 'DateIssue', 'DateEnd']),
+            'registration': ('Registration', ['DateIssue', 'DateEnd']),
+            'patent': ('Patent', ['Series', 'Number', 'IssuedWhom', 'TerritoryAction', 'DateIssue', 'DateEnd'])
+        }
+
         for doc in documents:
             if doc.type_document == 'passport':
-                passport = ET.SubElement(documents_worker, 'Passport', attrib={'Index': '0'})
-
-                series = ET.SubElement(passport, 'Series', attrib={'Index': '0'})
-                series.text = doc.series
-
-                number = ET.SubElement(passport, 'Number', attrib={'Index': '0'})
-                number.text = doc.number
-
-                issued_whom = ET.SubElement(passport, 'IssuedWhom', attrib={'Index': '0'})
-                issued_whom.text = doc.issued_whom
-
-                date_issue = str(doc.date_issue).split('-')
-                date_issue = f'{date_issue[2]}.{date_issue[1]}.{date_issue[0]}'
-                el_date_issue = ET.SubElement(passport, 'DateIssue', attrib={'Index': '0'})
-                el_date_issue.text = date_issue
-
-                date_end = str(doc.date_end).split('-')
-                date_end = f'{date_end[2]}.{date_end[1]}.{date_end[0]}'
-                el_date_end = ET.SubElement(passport, 'DateEnd', attrib={'Index': '0'})
-                el_date_end.text = date_end
-            elif doc.type_document == 'migration_card':
-                migration_card = ET.SubElement(documents_worker, 'MigrationCard', attrib={'Index': '0'})
-
-                series = ET.SubElement(migration_card, 'Series', attrib={'Index': '0'})
-                series.text = doc.series
-
-                number = ET.SubElement(migration_card, 'Number', attrib={'Index': '0'})
-                number.text = doc.number
-
-                date_issue = str(doc.date_issue).split('-')
-                date_issue = f'{date_issue[2]}.{date_issue[1]}.{date_issue[0]}'
-                el_date_issue = ET.SubElement(migration_card, 'DateIssue', attrib={'Index': '0'})
-                el_date_issue.text = date_issue
-
-                date_end = str(doc.date_end).split('-')
-                date_end = f'{date_end[2]}.{date_end[1]}.{date_end[0]}'
-                el_date_end = ET.SubElement(migration_card, 'DateEnd', attrib={'Index': '0'})
-                el_date_end.text = date_end
-            elif doc.type_document == 'registration':
-                registration = ET.SubElement(documents_worker, 'Registration', attrib={'Index': '0'})
-
-                date_issue = str(doc.date_issue).split('-')
-                date_issue = f'{date_issue[2]}.{date_issue[1]}.{date_issue[0]}'
-                el_date_issue = ET.SubElement(registration, 'DateIssue', attrib={'Index': '0'})
-                el_date_issue.text = date_issue
-
-                date_end = str(doc.date_end).split('-')
-                date_end = f'{date_end[2]}.{date_end[1]}.{date_end[0]}'
-                el_date_end = ET.SubElement(registration, 'DateEnd', attrib={'Index': '0'})
-                el_date_end.text = date_end
-            elif doc.type_document == 'patent':
-                patent = ET.SubElement(documents_worker, 'Patent', attrib={'Index': '0'})
-
-                series = ET.SubElement(patent, 'Series', attrib={'Index': '0'})
-                series.text = doc.series
-
-                number = ET.SubElement(patent, 'Number', attrib={'Index': '0'})
-                number.text = doc.number
-
-                issued_whom = ET.SubElement(patent, 'IssuedWhom', attrib={'Index': '0'})
-                issued_whom.text = doc.issued_whom
-
-                territory_action = ET.SubElement(patent, 'TerritoryAction')
-                territory_action.text = doc.territory_action
-
-                date_issue = str(doc.date_issue).split('-')
-                date_issue = f'{date_issue[2]}.{date_issue[1]}.{date_issue[0]}'
-                el_date_issue = ET.SubElement(patent, 'DateIssue', attrib={'Index': '0'})
-                el_date_issue.text = date_issue
-
-                date_end = str(doc.date_end).split('-')
-                date_end = f'{date_end[2]}.{date_end[1]}.{date_end[0]}'
-                el_date_end = ET.SubElement(patent, 'DateEnd', attrib={'Index': '0'})
-                el_date_end.text = date_end
-            elif doc.type_document == 'paycheck':
-                paycheck = ET.SubElement(documents_worker, 'Paycheck', attrib={'Index': '0'})
-
-                date_end = str(doc.date_end).split('-')
-                date_end = f'{date_end[2]}.{date_end[1]}.{date_end[0]}'
-                el_date_end = ET.SubElement(paycheck, 'DateEnd', attrib={'Index': '0'})
-                el_date_end.text = date_end
-            elif doc.type_document == 'temporary_residence':
-                temporary_residence = ET.SubElement(documents_worker, 'TemporaryResidence', attrib={'Index': '0'})
-
-                series = ET.SubElement(temporary_residence, 'Series', attrib={'Index': '0'})
-                series.text = doc.series
-
-                number = ET.SubElement(temporary_residence, 'Number', attrib={'Index': '0'})
-                number.text = doc.number
-
-                issued_whom = ET.SubElement(temporary_residence, 'IssuedWhom', attrib={'Index': '0'})
-                issued_whom.text = doc.issued_whom
-
-                date_issue = str(doc.date_issue).split('-')
-                date_issue = f'{date_issue[2]}.{date_issue[1]}.{date_issue[0]}'
-                el_date_issue = ET.SubElement(temporary_residence, 'DateIssue', attrib={'Index': '0'})
-                el_date_issue.text = date_issue
-
-                date_end = str(doc.date_end).split('-')
-                date_end = f'{date_end[2]}.{date_end[1]}.{date_end[0]}'
-                el_date_end = ET.SubElement(temporary_residence, 'DateEnd', attrib={'Index': '0'})
-                el_date_end.text = date_end
-            elif doc.type_document == 'residence_permit':
-                residence_permit = ET.SubElement(documents_worker, 'ResidencePermit', attrib={'Index': '0'})
-
-                series = ET.SubElement(residence_permit, 'Series', attrib={'Index': '0'})
-                series.text = doc.series
-
-                number = ET.SubElement(residence_permit, 'Number', attrib={'Index': '0'})
-                number.text = doc.number
-
-                issued_whom = ET.SubElement(residence_permit, 'IssuedWhom', attrib={'Index': '0'})
-                issued_whom.text = doc.issued_whom
-
-                date_issue = str(doc.date_issue).split('-')
-                date_issue = f'{date_issue[2]}.{date_issue[1]}.{date_issue[0]}'
-                el_date_issue = ET.SubElement(residence_permit, 'DateIssue', attrib={'Index': '0'})
-                el_date_issue.text = date_issue
-
-                date_end = str(doc.date_end).split('-')
-                date_end = f'{date_end[2]}.{date_end[1]}.{date_end[0]}'
-                el_date_end = ET.SubElement(residence_permit, 'DateEnd', attrib={'Index': '0'})
-                el_date_end.text = date_end
-            elif doc.type_document == 'certificate_asylum':
-                certificate_asylum = ET.SubElement(documents_worker, 'CertificateAsylum', attrib={'Index': '0'})
-
-                series = ET.SubElement(certificate_asylum, 'Series', attrib={'Index': '0'})
-                series.text = doc.series
-
-                number = ET.SubElement(certificate_asylum, 'Number', attrib={'Index': '0'})
-                number.text = doc.number
-
-                issued_whom = ET.SubElement(certificate_asylum, 'IssuedWhom', attrib={'Index': '0'})
-                issued_whom.text = doc.issued_whom
-
-                date_issue = str(doc.date_issue).split('-')
-                date_issue = f'{date_issue[2]}.{date_issue[1]}.{date_issue[0]}'
-                el_date_issue = ET.SubElement(certificate_asylum, 'DateIssue', attrib={'Index': '0'})
-                el_date_issue.text = date_issue
-
-                date_end = str(doc.date_end).split('-')
-                date_end = f'{date_end[2]}.{date_end[1]}.{date_end[0]}'
-                el_date_end = ET.SubElement(certificate_asylum, 'DateEnd', attrib={'Index': '0'})
-                el_date_end.text = date_end
-            elif doc.type_document == 'SNILS':
-                SNILS = ET.SubElement(documents_worker, 'SNILS', attrib={'Index': '0'})
-
-                number = ET.SubElement(SNILS, 'Number', attrib={'Index': '0'})
-                number.text = doc.number
-            elif doc.type_document == 'INN':
-                INN = ET.SubElement(documents_worker, 'INN', attrib={'Index': '0'})
-
-                number = ET.SubElement(INN, 'Number', attrib={'Index': '0'})
-                number.text = doc.number
+                document_type = doc.type_document
+                if document_type in documents_dict:
+                    document_element = ET.SubElement(documents_worker, documents_dict[document_type][0],
+                                                     attrib={'Index': '0'})
+                    for element_name in documents_dict[document_type][1]:
+                        element_value = getattr(doc, element_name.lower(), None)
+                        if element_value:
+                            element = ET.SubElement(document_element, element_name, attrib={'Index': '0'})
+                            if element_name.startswith('Date'):
+                                element_value = str(element_value).split('-')
+                                element_value = f'{element_value[2]}.{element_value[1]}.{element_value[0]}'
+                            element.text = element_value
 
     xml_string = minidom.parseString(ET.tostring(root)).toprettyxml(indent='  ', newl='\n')
     return xml_string
