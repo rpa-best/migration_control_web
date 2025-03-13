582274c50062d1efec07b937a52f5aac6b26d444
ProgrammSkill <antonkant2018@gmail.com>
2025-03-13 14:19:03 +0300

    Добавлено русское название для админ панели

--- v1_1/models/common.py
+++ v1_1/models/common.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Country(models.Model):
-    title = models.CharField(max_length=250)
+    title = models.CharField(max_length=250, verbose_name='Название')
 
     class Meta:
         verbose_name = 'Страну'
